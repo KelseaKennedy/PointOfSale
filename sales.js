8f343bb13c3bcbd6f0d89cdeac8b62e313486372
Kelsea <kkennedy27@mail.csuchico.edu>
2016-11-01 00:20:53 +0000

    set cookies

--- sales.js
+++ sales.js
@@ -4,21 +4,24 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-
+  
+document.getElementById("price").value = "";
+  
  if (isNaN(newItem) ) {
       window.alert("Enter price as a number."); 
  }
-
-  
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
+ else 
+ {
+ newItem = Number(newItem);
+ runningTotal = (runningTotal + newItem);
+ var dollars; 
+ dollars =  asCurrency(runningTotal);
+ 
+ document.getElementById("subtotal").innerHTML = dollars; 
+ setCookie("preTax", runningTotal, 20);
+ }
   //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
+ 
   // update a cookie called "preTax" with the value of runningTotal
 }
 
